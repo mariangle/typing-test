65842d1df060937c8b64a8aad828571566c0ba2b
Maria <mariale171001@gmail.com>
2023-05-21 16:30:57 +0200

    feat: navigate to site after logging in succesfully

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -5,3 +5,13 @@ import { SessionProvider } from "next-auth/react"
 interface AuthContextProps {
     children: React.ReactNode;
 }
+
+export default function AuthContext({
+    children
+}: AuthContextProps) {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
